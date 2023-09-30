4d205503a8e8416c1b52afa1929e3c35933db151
StecBartlomiej <stecb@student.agh.edu.pl>
2023-09-30 23:34:27 +0200

    Added basic cell tests

--- tests/SudokuModelCell.cpp
+++ tests/SudokuModelCell.cpp
@@ -1,7 +1,48 @@
 #include <gtest/gtest.h>
 
+#include "SudokuModel.hpp"
 
-TEST(Test1, IsTrue)
+
+TEST(CellTest, DeleteValue)
 {
-    EXPECT_TRUE(true);
+    Cell cell(2, true);
+
+    ASSERT_TRUE(cell.hasValue());
+    ASSERT_TRUE(cell.isModifiable());
+
+    cell.deleteValue();
+
+    EXPECT_FALSE(cell.hasValue());
+    EXPECT_TRUE(cell.isModifiable());
+}
+
+TEST(CellDeathTest, DeleteValue)
+{
+    Cell cell(2);
+
+    ASSERT_TRUE(cell.hasValue());
+    ASSERT_FALSE(cell.isModifiable());
+
+    EXPECT_DEATH_IF_SUPPORTED(cell.deleteValue(), "");
+}
+
+TEST(CellTest, SetValue)
+{
+    Cell cell(3, true);
+
+    ASSERT_EQ(cell.Value(), 3);
+    ASSERT_TRUE(cell.isModifiable());
+
+    cell.setValue(13);
+    EXPECT_EQ(cell.Value(), 13);
+}
+
+TEST(CellDeathTest, SetValue)
+{
+    Cell cell(3);
+
+    ASSERT_EQ(cell.Value(), 3);
+    ASSERT_FALSE(cell.isModifiable());
+
+    EXPECT_DEATH_IF_SUPPORTED(cell.setValue(13), "");
 }
