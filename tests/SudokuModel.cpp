eed5abdc463cd63dddcfdcaeeb2b5d4f08ff0ac2
StecBartlomiej <stecb@student.agh.edu.pl>
2023-10-01 22:59:29 +0200

    Added some test to SudokuModel

---
+++ tests/SudokuModel.cpp
@@ -0,0 +1,50 @@
+#include <gtest/gtest.h>
+
+#include "SudokuModel.hpp"
+
+
+TEST(SudokuModelTest, RowCountAndCollumnCount)
+{
+    SudokuModel model{};
+
+    // Invalid idx - RowCount and ColumnCount are independent of idx
+    QModelIndex idx{};
+
+    ASSERT_EQ(model.rowCount(idx), 9);
+    ASSERT_EQ(model.columnCount(idx), 9);
+}
+
+TEST(SudokuModelTest, SetData)
+{
+    SudokuModel model{};
+
+    auto idx = model.index(2, 2);
+
+    EXPECT_TRUE(model.setData(idx, QVariant(2)));
+    EXPECT_TRUE(model.setData(idx, QVariant(2u)));
+    EXPECT_TRUE(model.setData(idx, QVariant("2")));
+    EXPECT_TRUE(model.setData(idx, QVariant(2), Qt::EditRole));
+
+    QModelIndex invalidIdx{};
+
+    EXPECT_FALSE(model.setData(invalidIdx, QVariant(2)));
+    EXPECT_FALSE(model.setData(idx, QVariant()));
+    EXPECT_FALSE(model.setData(idx, QVariant(2.f)));
+    EXPECT_FALSE(model.setData(idx, QVariant(2), Qt::DisplayRole));
+}
+
+TEST(SudokuModelTest, Data)
+{
+    SudokuModel model{};
+
+    auto idx = model.index(2, 2);
+
+    model.setData(idx, QVariant(2));
+
+    EXPECT_EQ(model.data(idx), QVariant("2"));
+
+    QModelIndex invalidIdx{};
+    EXPECT_EQ(model.data(invalidIdx), QVariant{});
+
+    EXPECT_EQ(model.data(idx, Qt::TextAlignmentRole), Qt::AlignCenter);
+}
